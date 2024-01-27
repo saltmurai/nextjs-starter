0cd4aa2631fd8271a0af32c1ff354b6a0a9a4b3a
saltmurai <phamsang6260@gmail.com>
2024-01-27 19:07:16 +0700

    feat: update mock data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,73 @@
-import { Button, Container, Group } from "@mantine/core";
+import { Container } from "@mantine/core";
+
+const starSign = [
+  {
+    sign: "Aries",
+    horoscope:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque aut, vero, voluptatem hic maiores provident a dolor repellendus ex sint error cum sapiente eligendi harum, eaque cupiditate sunt omnis!",
+  },
+  {
+    sign: "Taurus",
+    horoscope:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque aut, vero, voluptatem hic maiores provident a dolor repellendus ex sint error cum sapiente eligendi harum, eaque cupiditate sunt omnis!",
+  },
+  {
+    sign: "Gemini",
+    horoscope:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque aut, vero, voluptatem hic maiores provident a dolor repellendus ex sint error cum sapiente eligendi harum, eaque cupiditate sunt omnis!",
+  },
+  {
+    sign: "Cancer",
+    horoscope:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque aut, vero, voluptatem hic maiores provident a dolor repellendus ex sint error cum sapiente eligendi harum, eaque cupiditate sunt omnis!",
+  },
+  {
+    sign: "Leo",
+    horoscope:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque aut, vero, voluptatem hic maiores provident a dolor repellendus ex sint error cum sapiente eligendi harum, eaque cupiditate sunt omnis!",
+  },
+  {
+    sign: "Virgo",
+    horoscope:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque aut, vero, voluptatem hic maiores provident a dolor repellendus ex sint error cum sapiente eligendi harum, eaque cupiditate sunt omnis!",
+  },
+  {
+    sign: "Libra",
+    horoscope:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque aut, vero, voluptatem hic maiores provident a dolor repellendus ex sint error cum sapiente eligendi harum, eaque cupiditate sunt omnis!",
+  },
+  {
+    sign: "Scropio",
+    horoscope:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque aut, vero, voluptatem hic maiores provident a dolor repellendus ex sint error cum sapiente eligendi harum, eaque cupiditate sunt omnis!",
+  },
+  {
+    sign: "Sagittarius",
+    horoscope:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque aut, vero, voluptatem hic maiores provident a dolor repellendus ex sint error cum sapiente eligendi harum, eaque cupiditate sunt omnis!",
+  },
+  {
+    sign: "Capicorn",
+    horoscope:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque aut, vero, voluptatem hic maiores provident a dolor repellendus ex sint error cum sapiente eligendi harum, eaque cupiditate sunt omnis!",
+  },
+  {
+    sign: "Aquarius",
+    horoscope:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque aut, vero, voluptatem hic maiores provident a dolor repellendus ex sint error cum sapiente eligendi harum, eaque cupiditate sunt omnis!",
+  },
+  {
+    sign: "Pisces",
+    horoscope:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque aut, vero, voluptatem hic maiores provident a dolor repellendus ex sint error cum sapiente eligendi harum, eaque cupiditate sunt omnis!",
+  },
+];
 
 export default function IndexPage() {
   return (
-    <Container fluid bg={"blue"}>
-      <div className="bg-red-500">ABCD</div>
-    </Container>
+    <main className="w-full text-white h-screen bg-black">
+      {/* Container */}
+      <div className="w-4/5 p-4 h-full mx-auto"></div>
+    </main>
   );
 }
