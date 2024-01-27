695e578def300d0e98a59f68c2febaff27db0b12
saltmurai <phamsang6260@gmail.com>
2024-01-27 20:56:07 +0700

    feat: modal opening

--- components/AstrologyCard.jsx
+++ components/AstrologyCard.jsx
@@ -1,5 +1,6 @@
 import useSWR from "swr";
-import { LoadingOverlay } from "@mantine/core";
+import { LoadingOverlay, Modal } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -8,16 +9,35 @@ function AstrologyCard({ sign, horoscope, icon }) {
     `${process.env.NEXT_PUBLIC_API_URL}/daily/${sign}`,
     fetcher
   );
+
+  const [opened, { open, close }] = useDisclosure(false);
+  function handleClick() {
+    open();
+  }
   return (
-    <div className="relative w-48 h-52 flex flex-col border rounded-md bg-zinc-800 p-2">
-      <div className="flex items-center justify-center">{icon}</div>
-      <div className="text-center text-xl font-semibold">{sign}</div>
-      {isLoading ? (
-        <LoadingOverlay visible={isLoading}></LoadingOverlay>
-      ) : (
-        <div className="text-sm mt-4 line-clamp-4">{data.horoscope}</div>
-      )}
-    </div>
+    <>
+      <div
+        onClick={handleClick}
+        className="relative hover:cursor-pointer hover:shadow hover:shadow-slate-300 w-48 h-52 flex flex-col border rounded-md bg-zinc-800 p-2"
+      >
+        <div className="flex items-center justify-center">{icon}</div>
+        <div className="text-center text-xl font-semibold">{sign}</div>
+        {isLoading ? (
+          <LoadingOverlay visible={isLoading}></LoadingOverlay>
+        ) : (
+          <div className="text-sm mt-4 line-clamp-4">{data.horoscope}</div>
+        )}
+      </div>
+
+      <Modal opened={opened} onClose={close} title={sign.toUpperCase()}>
+        <div className="flex items-center justify-center">{icon}</div>
+        {isLoading ? (
+          <LoadingOverlay visible={isLoading}></LoadingOverlay>
+        ) : (
+          <div className="text-sm mt-4">{data.horoscope}</div>
+        )}
+      </Modal>
+    </>
   );
 }
 
